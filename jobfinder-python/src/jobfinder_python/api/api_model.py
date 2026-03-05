6e1bf0140989a1346dc62ef098e9b354e6d25d7e
Nac31 <florianbenac46@gmail.com>
2026-03-05 20:46:34 +0100

    Search Jobs with google serp api

--- jobfinder-python/src/jobfinder_python/api/api_model.py
+++ jobfinder-python/src/jobfinder_python/api/api_model.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from fastapi import UploadFile
+from typing import Any
 
 class FindJobsDto(BaseModel):
     job_title: str
     job_location: str
     applicant_description: str
-    # cv: UploadFile
+    cv: UploadFile
 
 class Job(BaseModel):
     title: str
@@ -19,6 +20,8 @@ class Job(BaseModel):
     company_logo: str | None = None
     company_url: str | None = None
     company_description: str | None = None
+    extensions: list[str] | None = None
+    raw: Any
 
 class FindJobsResponse(BaseModel):
     jobs: list[Job]
